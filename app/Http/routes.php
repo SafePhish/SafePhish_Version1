accdb46bfc9c7e54a70bd53649623d6f3f299750
Throckmorton <tylerthrock95@gmail.com>
2016-08-04 13:58:56 -0400

    Added new routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,12 @@
 <?php
 
-Route::get('/test/{id}','EmailController@retrieveProjects');
+
+Route::get('/login','GUIController@generateLogin')->name('login');
+Route::post('/login','AuthController@authenticate');
+
+Route::get('/register','GUIController@generateRegister')->name('register');
+Route::post('/register','AuthController@create');
+
+Route::get('/',function() {
+    return view('displays.showReports'); //works for now - prefer to move to GUIController
+})->name('authHome');
